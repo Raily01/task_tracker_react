28f0181e5732a013a4e81ed3da88972a88fe30f9
Raily01 <shaykhulova.railya@gmail.com>
2023-02-27 16:41:54 +0300

    develop checked files

--- src/App.js
+++ src/App.js
@@ -2,20 +2,8 @@ function App() {
   const logo = 'https://cdn.dribbble.com/userupload/3081414/file/original-f2c23ce46b3a0c02d18be51cd8f494c4.png?resize=400x0'
   return (
     <div className="App">
-      <header className="App-header">
+    <h1>TASK_TRACKER</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
