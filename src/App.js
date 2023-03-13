182b355a7c8e2482f5c95b99f91f0defa5347349
Raily01 <shaykhulova.railya@gmail.com>
2023-03-13 12:20:14 +0300

    added font, eslint, prettier, husky

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   const logo = 'https://cdn.dribbble.com/userupload/3081414/file/original-f2c23ce46b3a0c02d18be51cd8f494c4.png?resize=400x0'
   return (
     <div className="App">
-    <h1>TASK_TRACKER</h1>
+    <h1>task_trackerg</h1>
         <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import "reset-css";
+ 
+export default createGlobalStyle`
+* {
+  font-family: 'Nunito Sans'!important;
+  box-sizing: border-box;
+}
+`;
+
