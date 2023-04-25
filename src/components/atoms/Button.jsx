f522c47a535b01d22db6dd84be5934055fb0f29e
Raily01 <shaykhulova.railya@gmail.com>
2023-04-25 20:16:02 +0300

    added destroy modal window hw7

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -14,6 +14,7 @@ const StyledButt = styled.button(
     display: block;
     position: relative;
     width: 100px;
+    border-radius: 8px;
   `,
 );
 
--- src/components/molecules/Table/Table.jsx
+++ src/components/molecules/Table/Table.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import styled from "styled-components";
 import "reset-css";
 import Button from "../../atoms/Button";
 import useProjects from "../../../lib/hooks/states/project";
+import ModalWindow from "../../atoms/ModalWindow";
 
 const StyledTable = styled.table`
   top: 0;
@@ -21,12 +23,14 @@ const StyledTh = styled.th`
   padding: 5px;
   background: #efefef;
   border: 2px solid purple;
+  font-size: 28px;
 `;
 
 const StyledTd = styled.td`
   border: 1px solid #dddddd;
   padding: 5px;
   text-align: center;
+  font-size: 20px;
 `;
 const Table = () => {
   const { projects, loading } = useProjects();
@@ -34,6 +38,7 @@ const Table = () => {
     console.log("pupupu");
   };
   const navigate = useNavigate();
+  const [isDestroyModalOpen, setDestroyModalOpen] = useState(false);
   const routeChange = (id1) => {
     navigate(`/tasks/${id1}`);
   };
@@ -51,20 +56,23 @@ const Table = () => {
               <StyledTh>&nbsp;</StyledTh>
             </tr>
             {projects.map(({ name, description, createdAt, id }) => (
-              <tr key={id}>
-                <StyledTd>{name}</StyledTd>
-                <StyledTd>{description}</StyledTd>
-                <StyledTd>{new Date(createdAt).toLocaleDateString("RU-ru")}</StyledTd>
-                <StyledTd>
-                  <Button label="Tasks" onClick={() => routeChange(id)} color="thistle" />
-                </StyledTd>
-                <StyledTd>
-                  <Button label="Edit" onClick={test} color="thistle" />
-                </StyledTd>
-                <StyledTd>
-                  <Button label="Destroy" onClick={test} color="thistle" />
-                </StyledTd>
-              </tr>
+              <>
+                <tr key={id}>
+                  <StyledTd>{name}</StyledTd>
+                  <StyledTd>{description}</StyledTd>
+                  <StyledTd>{new Date(createdAt).toLocaleDateString("RU-ru")}</StyledTd>
+                  <StyledTd>
+                    <Button label="Tasks" onClick={() => routeChange(id)} color="thistle" />
+                  </StyledTd>
+                  <StyledTd>
+                    <Button label="Edit" onClick={test} color="thistle" />
+                  </StyledTd>
+                  <StyledTd>
+                    <Button label="Destroy" onClick={() => setDestroyModalOpen(true)} color="thistle" />
+                  </StyledTd>
+                </tr>
+                {isDestroyModalOpen && <ModalWindow isOpen={isDestroyModalOpen} setIsOpen={setDestroyModalOpen} />}
+              </>
             ))}
           </tbody>
         </StyledTable>
